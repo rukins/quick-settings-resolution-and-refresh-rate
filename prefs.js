de4de7c6b54527edad9b87d6b706db481e6bc2ba
Sergey Rukin <sergey.rukin1425@gmail.com>
2024-07-13 23:40:41 +0500

    update preferences

--- prefs.js
+++ prefs.js
@@ -5,14 +5,36 @@ import Adw from 'gi://Adw';
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 
-export default class AutohideBatteryPercentagePreferences extends ExtensionPreferences {
+export default class QuickSettingsResolutionAndRefreshRatePreferences extends ExtensionPreferences {
 
     constructor(metadata) {
         super(metadata);
     }
 
     fillPreferencesWindow(window) {
-        
+        const settings = this.getSettings();
+        window._settings = settings;
+
+        const page = new Adw.PreferencesPage();
+        window.add(page);
+
+        const group = new Adw.PreferencesGroup();
+        page.add(group);
+
+        const addResolutionToggleMenuRow = new Adw.SwitchRow({
+            title: _("Add Resolution toggle menu to Quick Settings"),
+            active: settings.get_boolean("add-resolution-toggle-menu")
+        });
+        group.add(addResolutionToggleMenuRow);
+
+        const addRefreshRateToggleMenuRow = new Adw.SwitchRow({
+            title: _("Add Refresh Rate toggle menu to Quick Settings"),
+            active: settings.get_boolean("add-refresh-rate-toggle-menu")
+        });
+        group.add(addRefreshRateToggleMenuRow);
+
+        window._settings.bind("add-resolution-toggle-menu", addResolutionToggleMenuRow, "active", Gio.SettingsBindFlags.DEFAULT);
+        window._settings.bind("add-refresh-rate-toggle-menu", addRefreshRateToggleMenuRow, "active", Gio.SettingsBindFlags.DEFAULT);
     }
 
 }
